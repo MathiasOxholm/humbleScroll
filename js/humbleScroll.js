7a23d91d4084b355d4eaa35c4ac357ce18245b2c
Mathias Oxholm <mo.micheelsen@gmail.com>
2022-10-15 14:44:15 +0200

    Added: Callback observer

--- js/humbleScroll.js
+++ js/humbleScroll.js
@@ -4,9 +4,11 @@ const prefix = 'hs'
 const defaultOptions = {
   root: null,
   element: `[data-${prefix}]`,
-  visibleClass: `${prefix}-inview`,
-  threshold: 0.15,
-  offset: '0px',
+  enableCallback: false,
+  callback: `[data-${prefix}-call]`,
+  class: `${prefix}-inview`,
+  threshold: 0.1,
+  offset: '-32px 0px -32px 0px',
   repeat: false,
   startEvent: 'DOMContentLoaded',
 }
@@ -28,6 +30,8 @@ class HumbleScroll {
       rootMargin: this.options.offset,
       threshold: this.options.threshold,
     }
+    this.animationElements = document.querySelectorAll(this.options.element)
+    this.callbackElements = document.querySelectorAll(this.options.callback)
     this.init()
   }
 
@@ -36,13 +40,13 @@ class HumbleScroll {
     const event = new Event(`${prefix}-complete`, {
       bubbles: true,
       cancelable: true,
-      composed: false
+      composed: false,
     })
 
-    const observerFunction = (entries) => {
+    // Main animation function
+    const animationObserverFunction = (entries) => {
       entries.forEach((entry) => {
-
-        entry.target.classList.toggle(this.options.visibleClass, entry.isIntersecting)
+        entry.target.classList.toggle(this.options.class, entry.isIntersecting)
 
         if (isIntersectingFromTop(entry)) {
           entry.target.classList.add(`${prefix}-from-top`)
@@ -50,18 +54,24 @@ class HumbleScroll {
           entry.target.classList.add(`${prefix}-from-bottom`)
         }
 
-        if (entry.target.getAttribute(`data-${prefix}-call`) && entry.intersectionRatio > 0) {
-          const call = entry.target.getAttribute(`data-${prefix}-call`)
-          window[call]()
-        }
-
         if (!this.options.repeat && entry.isIntersecting) observer.unobserve(entry.target)
+      })
+    }
+
+    // Main callback function
+    const callbackObserverFunction = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const call = entry.target.getAttribute(`data-${prefix}-call`)
+          window[call] && window[call]()
+          caller.unobserve(entry.target)
+        }
       })
     }
 
     // Run observer on start event
     const startEventCallback = () => {
-      document.querySelectorAll(this.options.element).forEach((element) => {
+      this.animationElements.forEach((element) => {
         let attrValues = element.attributes
 
         attrValues = [...attrValues].filter((attr) => {
@@ -71,7 +81,7 @@ class HumbleScroll {
         attrValues.forEach((attr) => {
           let attrName = attr.name.replace(`data-${prefix}-`, `${prefix}-`)
           let attrValue = attr.value
-          this.setCSSVariable(element, attrName, attrValue)
+          this.setCSSVariable(element.firstElementChild, attrName, attrValue)
         })
 
         observer.observe(element)
@@ -83,10 +93,19 @@ class HumbleScroll {
       window.dispatchEvent(event)
     }
 
-    // Intersection Observer
-    const observer = new IntersectionObserver(observerFunction, this.observerOptions)
+    // Run Caller on start event
+    const startCallbackFunction = () => {
+      this.callbackElements.forEach((element) => {
+        caller.observe(element)
+      })
+    }
+
+    // Intersection Observers
+    const observer = new IntersectionObserver(animationObserverFunction, this.observerOptions)
+    const caller = new IntersectionObserver(callbackObserverFunction, this.observerOptions)
 
     window.addEventListener(this.options.startEvent, startEventCallback)
+    this.options.enableCallback && window.addEventListener(this.options.startEvent, startCallbackFunction)
     window.dispatchEvent(event)
   }
 
@@ -111,7 +130,7 @@ class HumbleScroll {
 
   // Helper function to debug HumbleScroll
   debug() {
-    console.log("HumbleScroll Debug is active. Remember to disable it in production.")
+    console.log('HumbleScroll Debug is active. Remember to disable it in production.')
     console.table(this.options)
 
     const help = document.createElement('div')
@@ -120,8 +139,8 @@ class HumbleScroll {
     help.setAttribute('style', `position: fixed; top: 0; left: 0; z-index: 9999; ; width: 100%; height: 100%; display:block; opacity: 1; pointer-events: none; padding: ${this.options.offset.replaceAll('-', '')};`)
     helpInner.setAttribute('style', `width: 100%; height: 100%; border-top: 1px solid yellow; border-bottom: 1px solid yellow;`)
 
-    document.head.insertAdjacentHTML("beforeend", `<style>${this.options.element} >* {outline: 1px solid red; position:relative;} ${this.options.element} > *::before, ${this.options.element} > *::after { content: ""; position: absolute; top: 0; left: 0; background: red; width: 100%; height: ${this.options.threshold * 100}%;pointer-events: none; opacity:0.25;} ${this.options.element} > *::after{bottom:0; top: auto;}</style >`)
-    document.querySelector('body').appendChild(help);
-    help.appendChild(helpInner);
+    document.head.insertAdjacentHTML('beforeend', `<style>${this.options.element} >* {outline: 1px solid red; position:relative;} ${this.options.element} > *::before, ${this.options.element} > *::after { content: ""; position: absolute; top: 0; left: 0; background: red; width: 100%; height: ${this.options.threshold * 100}%;pointer-events: none; opacity:0.25;} ${this.options.element} > *::after{bottom:0; top: auto;}</style >`)
+    document.querySelector('body').appendChild(help)
+    help.appendChild(helpInner)
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 // Fire regular DSOS
 const scroll = new HumbleScroll({
   repeat: true,
+  enableCallback: true,
 })
 
 // Fire custom DSOS with custom options
 const myCustomScroll = new HumbleScroll({
   element: '.my-custom-element',
-  visibleClass: 'enter',
+  class: 'enter',
 })
 
-scroll.on('hs-complete', () => {
+/* scroll.on('hs-complete', () => {
   console.log('HumbleScroll is complete!')
-})
+}) */
+
+//scroll.debug()
+
+function firstCall() {
+  const card = document.querySelector('#callback-1')
+  const cardHeading = document.querySelector('#callback-1-heading')
+  card.style = 'background-color: purple; border-radius: 100%; transform: scale(0.75);'
+  setTimeout(() => {
+    cardHeading.innerHTML = 'Kinda cool right?'
+  }, 4000)
+}
+
+function secondCall() {
+  const section = document.querySelector('#callback-section')
+  section.style = 'background-color: white; color: #a3e635;'
+}
